366eb82ef72e05d54c562c7fe372b93b24571a1d
Shahnas-P <shahanasp9@gmail.com>
2025-01-30 07:42:01 +0530

    Build Navbar'

---
+++ components/Button.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import React from 'react'
+
+type ButtonProps = {
+    type: 'button'| 'submit',
+    title :string,
+    icon?:string,
+    variant : 'btn_dark_green'
+}
+
+const Button = ({type,title,icon,variant}:ButtonProps) => {
+  return (
+   <button className={`flexCenter ${variant} rounded-full gap-3 border `} type={type}>
+    {icon &&     <Image  width={20} height={20} src={icon} alt={title} />
+ }
+ <label className='bold-16 whitespace-nowrap'>{title}</label>
+   </button>
+  )
+}
+
+export default Button
---
+++ components/Navbar.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { NAV_LINKS } from '@/constants'
+import Button from './Button'
+const Navbar = () => {
+  return (
+    <nav className='border border-red-400  relative py-5 padding-container z-30 flexBetween max-container'>
+
+      <Link href={"/"}>
+      <Image src="/hilink-logo.svg" alt='Logo' width={74} height={29} />
+      </Link>
+      <ul className=' hidden lg:flex border border-green-300 h-full gap-12 '>
+{/* nav items */}
+{
+  NAV_LINKS.map((links)=>(
+    <Link className='regular-16 transition-all pb-1.5 hover:font-bold flexCenter text-gray-50 ' href={links.href} key={links.key}>{links.label} </Link>
+  ))
+}
+      </ul>
+{/* Button */}
+      <div className='border border-violet-400 hidden lg:flex '>
+<Button  type="button" title='Login' icon="/user.svg" variant="btn_dark_green" />
+      </div>
+
+      {/* Hamburger Menu for mobile devices */}
+      <div className='flex lg:hidden  '>
+        <Image className='cursor-pointer inline-block ' width={32} height={32} src='menu.svg' alt='menu' />
+      </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
